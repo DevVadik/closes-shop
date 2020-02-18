9d61701c98b844e1244f7d20af38c72c0c47512c
Unicorn <developervadik@gmail.com>
2020-02-18 12:28:54 +0300

    finishing sign-in handlesubmit

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -4,7 +4,7 @@ import './sign-in.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from './../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.util';
+import { signInWithGoogle, auth } from '../../firebase/firebase.util';
 
  class SignIn extends Component {
 
@@ -17,10 +17,21 @@ import { signInWithGoogle } from '../../firebase/firebase.util';
     }
   }
 
-  handleSubmit = (e) => {
-    e.prevenrDefault();
+  handleSubmit = async (e) => {
+    e.preventDefault();
 
-    this.setState({email: '', password: ''})
+    const {email,password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+
+      this.setState({email: '', password: ''})
+
+    } catch (error) {
+      console.error('error: ', error);
+    }
+
+
   }
 
   handleChange = (e) => {
