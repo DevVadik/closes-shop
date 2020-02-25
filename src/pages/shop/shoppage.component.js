e006248ce994c8865d9ab9df14ee4587d2aafa6c
Unicorn <developervadik@gmail.com>
2020-02-25 13:54:17 +0300

    implemented shoppage and directorycomponent to redux

--- src/pages/shop/shoppage.component.js
+++ src/pages/shop/shoppage.component.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from "reselect";
 
-
-import { SHOP_DATA } from './shop.data'
 import CollectionPreview from './../../components/collection-preview.component/collection-preview.component';
+import { selectShopItems } from '../../redux/shop/shop.selectors'
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections:  SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state
-
-    return (
-      <div className="shop-page">
-        {
-          collections.map(({id, ...otherCollectionProps}) => (
-            <CollectionPreview key={id} {...otherCollectionProps}/>
-          ))
-        }
-      </div>
-    )
-  }
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {
+        collections.map(({id, ...otherCollectionProps}) => (
+          <CollectionPreview key={id} {...otherCollectionProps}/>
+        ))
+      }
+    </div>
+  )
 }
 
-export default ShopPage
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopItems
+})
+
+export default connect(mapStateToProps)(ShopPage)
